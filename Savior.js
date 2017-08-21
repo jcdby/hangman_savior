1dd3d9ef1ebc1711b35267a7e3bff7d1a76a2f04
Jincheng Li <jcdby@chemessen.com>
2017-08-21 14:21:31 +0900

    添加计算字母出现次数函数和test

--- Savior.js
+++ Savior.js
@@ -24,8 +24,73 @@ class Savior {
 
   }
 
+  getNextLetterToGuess(dict) {
+    let nextLetter = 'b';
+    if(!Array.isArray(dict)){
+      throw new Error('The first parameter should be a array.');
+    }
+    if(dict.length === 0){
+      let backupletters = this.myKnowledge.backupFreq;
+      nextLetter = backupletters.charAt(0);
+      this.setBackupLetterFreq(backupletters.slice(1));
+      return nextLetter;
+    }
+
+    //随机设置一个字母表示字典里出现频率较高的字母。
+    let freq = {availableLetter: 'a'};
+
+    //遍历字典
+    dict.forEach((word) => {
+      if(typeof word !== 'string'){
+        throw new Error('Your dict contains non-string value!');
+      }
+      //每个单词里面所包含的字母集合
+      let lettersIncluded = {};
+      for(let i = 0; i < word.length; i++){
+        let letter = word.charAt(i);
+        //确保相同的字母只出现一次。
+        lettersIncluded[letter] = letter
+      }
+
+      //根据单词里出现的字母，增加字典里其字母出现的次数。
+      for(let key in lettersIncluded){
+        if(typeof freq[key] === 'number'){
+          //这个字母已经在之前单词出现过的情况。
+          freq[key]++;
+        }else{
+          //这个字母第一次出现的情况。
+          freq[key] = 1;
+        }
+
+        //由于初始化的时候指定availableLetter为a，但在字典中的单词里很有可能不包含a，所以检查其有效性。若此字母的出现的次数已经被计算，那么其类型应该为number。
+        if(!freq[freq.availableLetter] || typeof freq[freq.availableLetter] === 'number' && freq[key] > freq[freq.availableLetter]){
+          freq.availableLetter = key;
+        }
+      }
+    });
+
+
+    nextLetter = freq.availableLetter;
+
+    return nextLetter;
+  }
+
+
+  setBackupLetterFreq(backup_letter_freq) {
+    if(typeof backup_letter_freq !== 'string'){
+      throw new Error('The parameter should be a string!');
+    }
+    this.myKnowledge.backupFreq = backup_letter_freq;
+  }
+
+  getBackupLetterFreq() {
+    return this.myKnowledge.backupFreq;
+  }
 
   setLengthByWordToGuess(wordToGuess){
+    if(typeof wordToGuess !== 'string'){
+      throw new Error('The parameter of setLengthByWordToGuess should be a string.');
+    }
     this.length = wordToGuess.length;    
   }
 
@@ -36,7 +101,7 @@ class Savior {
 
   learnEnglish(path) {
     if(arguments.length === 0 || !path ){
-      throw Error('You should input a path!');
+      throw new Error('You should input a path!');
     }
     if(arguments.length === 1 && typeof path === 'string') {
       path = path.trim();
@@ -53,7 +118,7 @@ class Savior {
     this.myKnowledge = {
       dict: {},
       lettersFreq: [],
-      backupFreq: []
+      backupFreq: ''
     }
   }
 
@@ -99,11 +164,15 @@ class Savior {
     }
     let new_dict = [];
 
+
+    // 当parameter只有两个的时候，代表的是savior猜错了字母。使用这个错误的字母更新字典。
     if(arguments.length === 2){
       new_dict = old_dict.filter((el) => {
         return !el.includes(letter);
       })
     }
+
+    // 当parameter有3个的时候，代表的是savior猜正确了字母。使用这个正确的字母和其在单词中间的位置信息更新字典。
     if(arguments.length === 3){
       let reg = [];
       for(let i = 0; i < this.getLengthForWordToGuess(); i++){
--- Savior.test.js
+++ Savior.test.js
@@ -14,7 +14,7 @@ describe('Savior Test', ()=>{
   });
 
 
-  describe('Init test', () => {
+  describe('1. Init test', () => {
     test('Should Savior not null', () => {
       expect(savior).not.toBeNull();
     });
@@ -33,7 +33,7 @@ describe('Savior Test', ()=>{
   });
 
 
-  describe('Savior learns english', () => {
+  describe('2. Savior learns english', () => {
     test('Should throw a error when not to give learnEnglish method a parameter about path.', () => {
       expect(()=>{
         savior.learnEnglish()
@@ -71,12 +71,12 @@ describe('Savior Test', ()=>{
         expect(saviorKnowledge.dict).toBeInstanceOf(Object);
         expect(saviorKnowledge.lettersFreq).toBeInstanceOf(Array);
         expect(saviorKnowledge.backupFreq).not.toBeNull();
-        expect(saviorKnowledge.backupFreq).toBeInstanceOf(Array);
+        expect(typeof saviorKnowledge.backupFreq).toEqual('string');
     });
   });
 
 
-  describe('Savior updates his dict.', () => {
+  describe('3. Savior updates his dict.', () => {
     test('Should update savior`s dict by the length of the word to be gussed.', () => {
       let old_dict = ['good','a','bb','cooler','sdfjoijeifj','fjkdjjfd','ss','wordl', 'word','world'];
       //The length of the word to be guessed is 5. 
@@ -90,6 +90,13 @@ describe('Savior Test', ()=>{
         expect(new_dict[i]).toEqual(expect_dict[i]);
       }
     });
+
+
+    test('Should throw error when the parameter of the setLengthByWordToGuess is not a string.', () => {
+      expect(() => {
+        savior.setLengthByWordToGuess(2);
+      }).toThrowError('The parameter of setLengthByWordToGuess should be a string.')
+    })
 
 
     test('Should throw error when updateDictByWordLength`s first parameter is not array and second parameter is not Number.', () => {
@@ -163,9 +170,61 @@ describe('Savior Test', ()=>{
     });
   });
 
-  describe('Savior updates his collection about letter frequencies.', () => {
-    
-  }) 
+  describe('4. Savior updates the next letter to guess', () => {
+    test('Should return the letter which is appeared in most words.', () => {
+      let dict = ['gooad','gooa','boooooooooooooooooooooooooad','cooad','sbjad','fjkw','sbbe','worad', 'wooare','wooarl'];
+      let expect_letter = 'a'; //It counts only once when the same letters appeared in the same word
+
+      let next_letter = savior.getNextLetterToGuess(dict);
+      expect(next_letter).toEqual(expect_letter);
+
+    });
+
+    test('Should throw error when dict includes non-string value.', () => {
+      let dict = [2,3,4];
+      expect(() => {
+        let next_letter = savior.getNextLetterToGuess(dict);
+      }).toThrowError('Your dict contains non-string value!');
+    });
+
+
+    test('Should return the first character in the backup freq collection and update the backup freq collection when the dict updated is empty.', () =>{
+      let dict = [];
+      let backup_letter_freq = 'eabiqwr';
+      let updated_backup_letter_freq  = 'abiqwr';
+      let expect_next_letter = 'e';
+
+      savior.setBackupLetterFreq(backup_letter_freq);
+
+      let next_letter = savior.getNextLetterToGuess(dict);
+      expect(next_letter).toEqual(expect_next_letter);
+      expect(savior.getBackupLetterFreq()).toEqual(updated_backup_letter_freq);
+
+    });
+
+    test('Should throw error when the first parameter of the getNextLetterToGuess is not a valid array.', () => {
+      expect(() => {
+        savior.getNextLetterToGuess({});
+      }).toThrowError('The first parameter should be a array.');
+      expect(() => {
+        savior.getNextLetterToGuess([]);
+      }).not.toThrowError('The first parameter should be a array.');
+    });
+
+    test('Should set a backup collection about letter frequecy and get it', () => {
+      let backup_letter_freq = 'eabiqwr';
+      savior.setBackupLetterFreq(backup_letter_freq);
+      expect(savior.getBackupLetterFreq()).toEqual(backup_letter_freq);
+    });
+
+    test('Should throw error when the parameter of setBackupLetterFreq is not a string.', () => {
+      expect(() => {
+        savior.setBackupLetterFreq([]);
+      }).toThrowError('The parameter should be a string!');
+    })
+
+
+  }); 
 
 
 
